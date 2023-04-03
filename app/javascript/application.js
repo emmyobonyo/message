1955c1e0a242528ae7c6c0529b369ddf6387b1ba
obonyoemmy <emmanuel.obonyo@sureswiftcapital.com>
2023-04-03 12:23:22 +0300

    instal jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import '@doabit/semantic-ui-sass'
+import * as jquery from "jquery"
+import "semantic-ui"
+
+$(document).on('turbo:load', function() {
+  console.log('loaded turbo links')
+  $('.ui.dropdown').dropdown()
+});
