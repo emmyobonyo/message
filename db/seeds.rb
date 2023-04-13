34fd4a641ebf17b6af77f5ce97af57c3d8c5a8ba
obonyoemmy <emmanuel.obonyo@sureswiftcapital.com>
2023-04-13 12:10:57 +0300

    learn how to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: 'emmanuel', password: 'password')
+User.create(username: 'obonyo', password: 'password')
+User.create(username: 'toby', password: 'password')
+User.create(username: 'abel', password: 'password')
+User.create(username: 'somerandomuser', password: 'password')
